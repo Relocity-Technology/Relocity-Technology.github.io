b04896e4a02d094f3dc5a5f1b82a3812d8b1cbda
Harsh <harsh.raj8833@gmail.com>
2022-12-15 14:22:06 +0530

    added: bg component

--- src/ui/Cover.js
+++ src/ui/Cover.js
@@ -1,5 +1,4 @@
 import { Box, Typography } from '@mui/material';
-import { style } from '@mui/system';
 
 
 import React from 'react'
@@ -7,38 +6,50 @@ import Header from './Header';
 
 function Cover({ children }) {
   return (
-    <Box sx={{
+    <Box 
+    
+    style={{overflowX: "hidden"}}
+
+    sx={{
       width: '100%',
       height: 720,
       background: "linear-gradient(121.77deg, #F95738 16.55%, #FB8D28 79.06%);",
-
     }}>
       <Header />
-      <Box style={{paddingTop: "16%"}} sx={{
+      <Box style={{ paddingTop: "13%" }} sx={{
         textAlign: 'center'
-        
-      }}
-      >
-        <div style={{
-  
-        }}>
-          <Typography variant='h4' fontWeight={700} fontStyle={'italic'} color={'white'} lineHeight={'108.3%'}>
-            Delivery Services that connects
-          </Typography>
-          <Typography variant='h4' fontWeight={700} fontStyle={'italic'} color={'white'} lineHeight={'108.3%'}>
-            businesses to their customers
-          </Typography>
-          <Typography variant='h6' fontWeight={600}  color={'#DFCECB'} lineHeight={'108.3%'}>
-          An on-demand platform that offers delivery services for 
-          </Typography>
-          <Typography variant='h6' fontWeight={600}  color={'#DFCECB'} lineHeight={'108.3%'}>
-          small businesses, retailers, and brands  
-          </Typography>
-        </div>
-
+      }}>
+        <Typography variant='h3' fontWeight={700} fontStyle={'italic'} color={'white'} lineHeight={'108.3%'}>
+          Delivery Services that connects
+        </Typography>
+        <Typography variant='h3' fontWeight={700} fontStyle={'italic'} color={'white'} lineHeight={'108.3%'}>
+          businesses to their customers
+        </Typography>
+        <Typography paddingTop={'10px'} letterSpacing={1.5} variant='h6' fontWeight={600} color={'#FEEFE3'} lineHeight={'108.3%'}>
+        An on-demand platform that offers delivery services for small 
+        </Typography>
+        <Typography letterSpacing={1.5} variant='h6' fontWeight={600} color={'#FEEFE3'} lineHeight={'108.3%'}>
+        businesses, retailers, and brands
+        </Typography>
       </Box>
-      <>
-      </>
+      
+      {/* bg circular vector */}
+      <Box
+
+      sx={{
+        display: 'flex',
+        background: 'linear-gradient(90.76deg, rgba(253, 196, 83, 1) -36.18%, rgba(255, 212, 60, 0) 54.74%)',
+        borderRadius: '274.5px',
+          position: 'absolute',
+          right: '-110px',
+        width: "550px",
+        height: "550px",
+        transform: 'matrix(-0.12, 0.99, 0.99, 0.12, 0, 0)'
+
+        
+      }}>
+          
+      </Box>
     </Box>
   )
 }
--- src/ui/Header.js
+++ src/ui/Header.js
@@ -1,11 +1,40 @@
 import React from 'react'
+import { Typography, Grid } from '@mui/material';
+
 
 
 function Header() {
   return (
-    <div>
-      Header: Todo
-    </div>
+
+
+
+    <Grid style={{ paddingTop: "40px", paddingLeft: "40px", paddingRight: "40px" }} container spacing={2}>
+      <Grid xs={6}>
+        <Typography  color="white"  fontWeight={700}>
+          Relociy
+        </Typography>
+      </Grid>
+      <Grid xs={6} style={{paddingLeft: "100px"}}>
+        <Typography display="inline" style={{ paddingRight: "25px" }} color="white"  fontWeight={600}>
+          Home
+        </Typography>
+        <Typography display="inline" style={{ paddingRight: "25px" }} color="white"  fontWeight={600}>
+          About us
+        </Typography>
+        <Typography display="inline" style={{ paddingRight: "25px" }} color="white"  fontWeight={600}>
+          How it works?
+        </Typography>
+        <Typography display="inline" style={{ paddingRight: "25px" }} color="white"  fontWeight={600}>
+          For Business
+        </Typography>
+        <Typography display="inline" style={{ paddingRight: "25px" }} color="white"  fontWeight={600}>
+          Get in Touch
+        </Typography>
+      </Grid>
+    </Grid>
+
+
+
   )
 }
 
